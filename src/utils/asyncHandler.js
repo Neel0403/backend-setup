2e1bc015adf4b4749755574a5d6caa359b43edb8
Neel0403 <neelo2003@gmail.com>
2024-07-02 16:50:17 +0530

    Fix Bug

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,9 @@
 
 const asyncHandler = (requestHandler) => {
     (req, res, next) => {
-        Promise.resolve(requestHandler).catch((err) => next)
+        Promise
+            .resolve(requestHandler(req, res, next))
+            .catch((err) => next)
     }
 }
 
